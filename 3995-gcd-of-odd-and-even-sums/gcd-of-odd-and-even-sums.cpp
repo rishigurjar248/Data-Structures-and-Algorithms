ec358204da9488f3bd04838eda1080631a2f1fc3
RISHI.GURJAR <rishigurjar248@gmail.com>
2026-07-15 13:54:05 +0530

    Time: 279 ms (11.20%) | Memory: 8.7 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    int minimum(int a,int b)
+    {
+        if(a<b)
+            return a;
+        else
+            return b;
+    }
     int gcdOfOddEvenSums(int n) {
-        return n;
+        int sumOdd = n * n;
+        int sumEven = (n+1) * n;
+        int gcd = 1;
+
+        for(int i = 2;i<=(minimum(sumOdd,sumEven)/2);i++)
+        {
+            if((sumOdd%i==0) && (sumEven%i==0))
+            {
+                gcd = i;
+            }
+        }
+
+        return gcd;
     }
 };
