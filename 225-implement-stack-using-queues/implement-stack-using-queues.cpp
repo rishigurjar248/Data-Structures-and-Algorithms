5ea8f91867f98158b0d2f1d72f4314fe354634ad
RISHI.GURJAR <rishigurjar248@gmail.com>
2026-08-29 16:43:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (88.61%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,32 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+private:
+    queue<int> q;
+public:
+    MyStack() {}
+    
+    void push(int x) {
+        int size = q.size();
+        q.push(x);
+        for (int i = 0; i < size; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
