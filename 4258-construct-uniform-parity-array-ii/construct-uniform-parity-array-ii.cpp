3d91f312391fca31326357504853e7073220b115
RISHI.GURJAR <rishigurjar248@gmail.com>
2026-09-03 09:17:29 +0530

    Time: 1 ms (79.58%) | Memory: 165.8 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        return ranges::min(nums1)&1 || ranges::all_of(nums1, [](int x){ return (x&1)==0;});
+        int n=nums1.size(),mn=INT_MAX;
+        for (int i:nums1) {
+            if ((i%2) == 1) {
+                mn = min(mn, i);
+            }
+        }
+        if (mn == INT_MAX)return true;
+        for (int x : nums1) {
+            if (x % 2 == 0 && x < mn)
+            return false;
+        }
+        return true;
     }
 };
